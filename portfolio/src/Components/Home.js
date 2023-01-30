f255daa45e680ed8ce33b8469ddc09008597c05b
Lucas Aguiar <lucasabr2001@gmail.com>
2023-01-30 17:00:11 -0500

    Added photo/ finalized home

--- portfolio/src/Components/Home.js
+++ portfolio/src/Components/Home.js
@@ -2,8 +2,12 @@ import '../App.css';
 
 const Home = (props) => {
     return (
-        <div className="IntroBox">
-            <h1 className='Intro'>{props.message}</h1>
+        <div className="HomeBox">
+            <div className="IntroBox">
+                <h1 className='Intro'>{props.message}</h1>
+            </div>
+            <div className="vertical"></div>
+            <img src="./IMG_1623.jpeg" alt="Photo of Me" className='homePhoto'></img>
         </div>
     )};
 

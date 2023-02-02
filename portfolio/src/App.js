cac573dbcb43131bb19b011fe9c68aed5cf467de
Lucas Aguiar <lucasabr2001@gmail.com>
2023-02-02 17:05:00 -0500

    Add content to contact page (needs to be styled)

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -3,13 +3,14 @@ import Header from './Components/Header';
 import Home from './Components/Home';
 import Contact from './Components/Contact';
 import homeContent from './Content/home.json';
+import contactContent from './Content/contact.json';
 
 function App() {
   return (
     <div className="App">
         <Header />
         <Home message={homeContent.message} />
-        <Contact />
+        <Contact message={contactContent.message} links={contactContent.links}/>
     </div>
   );
 }
--- portfolio/src/Components/Contact.js
+++ portfolio/src/Components/Contact.js
@@ -1,9 +1,15 @@
 import '../App.css';
 
 const Contact = (props) => {
+    const email = `mailto:${props.links.email}`
     return (
         <div className="ContactBox">
-            <h1 className="Intro">I'd love to connect! Contact with me via email or linkedin!</h1>
+            <h1 className="Intro">{props.message}</h1>
+            <div className="LinksBox">
+                <img src="./icons8-linkedin-circled-96.png"><a target={props.links.linkedin}></a></img>
+                <img src="./icons8-github-100.png"><a target={props.links.github}></a></img>
+                <img src='./icons8-mail-100.png'><a target={email}></a></img>
+            </div>
         </div>
     )};
 
